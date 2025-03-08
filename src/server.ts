3898e2e1d6b41848e0d439e380663bcd745131f1
noa1608 <noa160802@gmail.com>
2025-03-08 19:37:20 +0200

    saving images on server

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import authRoutes from "./routes/users";
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
 import cors from 'cors';
+import path from "path";
+import multer from "multer";
 
 
 const app = express();
@@ -19,7 +21,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/posts", postsRoute);
 app.use("/comments", commentsRoute);
 app.use("/auth", authRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(upload.single("image")); 
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
