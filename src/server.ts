da230761f92f0a1aa7f8d90cbe1cc4b89d697d56
Linoy Pasinkov <161356608+linoypas@users.noreply.github.com>
2025-03-23 16:52:21 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ import session from "express-session";
 const app = express();
 app.use(
   cors({
-    origin: “https://node43.cs.colman.ac.il/“,
+    origin: "https://node43.cs.colman.ac.il/",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
