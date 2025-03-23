e85d44bfd7f8c1bd86db1dcbe19d24b8cb5cb96d
Linoy Pasinkov <161356608+linoypas@users.noreply.github.com>
2025-03-23 15:48:39 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,21 @@ import http from 'http'
 import fs from 'fs'
 import https from 'https'
 
+const keyPath = path.join(__dirname, 'client-key.pem');
+const certPath = path.join(__dirname, 'client-cert.pem');
+
+const options = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath)
+};
 initApp().then((app) => {
     if(process.env.NODE_ENV !== 'production') {
       console.log('developmnet')
     http.createServer(app).listen(process.env.PORT);
     }
     const option= {
-      key: fs.readFileSync('./client-key.pem'),
-      cert: fs.readFileSync('/client-cert.pem')
+      key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
     };
     https.createServer(option, app).listen(process.env.PORT);
   });
